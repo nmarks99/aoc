f66aca4931ad500738fa98f0a43b39ef2ef06425
Nick Marks <nmarks@anl.gov>
2023-12-04 13:10:51 -0600

    almost works, but not every time. eightwonine edge case

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,26 +1,32 @@
 use std::fs;
 use std::collections::HashMap;
 
-fn word_to_num(msg: &String) {
+fn word_to_num(msg: &String) -> String {
 
-    let mut word_nums: HashMap<&str, (u32, u32)> = HashMap::new();
-    word_nums.insert("one", (1,3));
-    word_nums.insert("two", (2,3));
-    word_nums.insert("three", (3,5));
-    word_nums.insert("four", (4,4));
-    word_nums.insert("five", (5,4));
-    word_nums.insert("six", (6,3));
-    word_nums.insert("seven", (7,5));
-    word_nums.insert("eight", (8,5));
-    word_nums.insert("nine", (9,4));
+    let mut word_nums: HashMap<&str, u32> = HashMap::new();
+    word_nums.insert("one", 1);
+    word_nums.insert("two", 2);
+    word_nums.insert("three", 3);
+    word_nums.insert("four", 4);
+    word_nums.insert("five", 5);
+    word_nums.insert("six", 6);
+    word_nums.insert("seven", 7);
+    word_nums.insert("eight", 8);
+    word_nums.insert("nine", 9);
+    
+    let mut string_out = msg.clone();
 
     for k in word_nums.keys() {
-        if msg.contains(k) {
-            let val = word_nums.get(k).unwrap().0;
-            let length = word_nums.get(k).unwrap().1;
-            println!("{} = {} of length {}",k,val,length);
+        let first = string_out.find(k);
+        if first.is_some() {
+            let val = word_nums.get(k).unwrap();
+            let length = k.len();
+            let i0 = first.unwrap();
+            let i1 = i0 + length;
+            string_out.replace_range(i0..i1, &val.to_string());
         }
     }
+    string_out
 }
 
 fn get_num(msg: &String) -> u32 {
@@ -49,10 +55,8 @@ fn get_num(msg: &String) -> u32 {
 }
 
 fn main() {
-    // let a = String::from("hellothree");
-    // word_to_num(&a);
 
-    let file_path: &str = "../files/day1.txt";
+    let file_path: &str = "../files/day1_2_test.txt";
     let cont : Vec<u8> = fs::read(file_path).expect("Could not read file");
 
     let mut line: String = Default::default();
@@ -70,9 +74,11 @@ fn main() {
 
     let mut total: u32 = 0;
     for line in data {
-        let v: u32 = get_num(&line);
-        // print!("line: {}\t", line);
-        // println!("value = {}", v);
+        println!("line = {}", line);
+        let line_out = word_to_num(&line);
+        let v: u32 = get_num(&line_out);
+        print!("line_out: {}\t", line_out);
+        println!("value = {}\n", v);
         total = total + v;
     }
 
