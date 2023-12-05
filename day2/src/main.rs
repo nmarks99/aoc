eafdb9ff60d9a7b542cc956da384144ed5d6a3d4
Nick Marks <nmarks@anl.gov>
2023-12-05 11:33:41 -0600

    day2 part1 done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 #![allow(unused_variables)]
+#![allow(unused_mut)]
 
 use std::fs::File;
 use std::io::Read;
@@ -10,6 +11,50 @@ use std::collections::HashMap;
 const N_RED: u8 = 12;
 const N_GREEN: u8 = 13;
 const N_BLUE: u8 = 14;
+
+
+fn get_id_from_line(line: &str) -> u16 {
+    
+    let mut id_string = String::new();
+    let colon_ind = line.find(":").unwrap();
+    for i in 5..colon_ind {
+        id_string.push(line.chars().nth(i).unwrap());
+    }
+    let id: u16 = id_string.parse().unwrap();
+    id
+}
+
+fn get_count_for_color(turn: &str, color_ind: usize) -> u8 {
+    
+    let mut i: usize = color_ind;
+    let mut comma_ind: usize;
+    // println!("turn = {}", turn);
+    // println!("Color ind = {}", color_ind);
+    loop {
+        let c = turn.chars().nth(i).unwrap();
+        // println!("char[{}] = {}", i, c);
+        if c.eq(&',') || i == 0 {
+            comma_ind = i;
+            // println!("stop at {}", i);
+            break;
+        }
+        else {
+            i = i - 1;
+        }
+    }
+    let mut num_string = String::new();
+    if comma_ind != 0 {
+        comma_ind += 1;
+    }
+    for j in comma_ind..color_ind {
+        num_string.push(turn.chars().nth(j).unwrap());
+    }
+    
+    // println!("num_string = {:?}", num_string);
+    let count: u8 = num_string.trim().parse().unwrap();
+    // println!("count = {}", count);
+    count
+}
 
 fn main() {
     
@@ -22,28 +67,31 @@ fn main() {
         .lines()
         .map(|c| c.to_string())
         .collect();
+   
     
     let colors = vec!["red", "green", "blue"];
     let mut total: u16 = 0;
 
     for game in &lines {
-        // let mut game: &str = &lines[0];
-        let id = game.chars().nth(5).unwrap().to_digit(10).unwrap() as u16;
-        println!("Game id = {}", id);
-        let game_str = &game[8..];
-        // println!("{}", game1);
+        // println!("---------------------------");
+        // println!("{}", game);
+        let id: u16 = get_id_from_line(&game);
+        // println!("id = {}", id);
+        let colon_ind = game.find(":").unwrap();
+        let game_str = &game[colon_ind+1..];
         let split_game: Vec<&str> = game_str.split(";").collect();
 
-        let mut possible = true;
+        let mut possible = false;
         for turn in &split_game {
+            // println!("Turn = {}", turn);
             let mut red_count: u8 = 0;
             let mut green_count: u8 = 0;
             let mut blue_count: u8 = 0;
-            // println!("Turn = {}", turn);
             for color in &colors {
                 let ind: Vec<_> = turn.match_indices(*color).collect();
                 for (i, v) in &ind {
-                    let value: u8 = turn.chars().nth(*i-2).unwrap().to_digit(10).unwrap() as u8;
+                    // println!("{}, {}", i, v);
+                    let value: u8 = get_count_for_color(&turn, *i);
                     match v {
                         &"red" => red_count = value,
                         &"green" => green_count = value,
@@ -54,11 +102,16 @@ fn main() {
             }
             if red_count > N_RED || green_count > N_GREEN || blue_count > N_BLUE {
                 possible = false;
+                // println!("Impossible");
                 break;
+            }
+            else {
+                possible = true;
+                // println!("Possible");
             }
         }
         if possible {
-            total = id as u16;
+            total = total + id;
         }
     }
     println!("total = {}", total);
