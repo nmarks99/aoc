aec0bbf256c0837277c97afde3dcb0f4f33f005e
Nick Marks <nmarks@anl.gov>
2023-12-05 15:00:01 -0600

    finished day2 part2 and reoganized stuff

--- day2/part2/src/main.rs
+++ day2/part2/src/main.rs
@@ -28,14 +28,10 @@ fn get_count_for_color(turn: &str, color_ind: usize) -> u8 {
     
     let mut i: usize = color_ind;
     let mut comma_ind: usize;
-    // println!("turn = {}", turn);
-    // println!("Color ind = {}", color_ind);
     loop {
         let c = turn.chars().nth(i).unwrap();
-        // println!("char[{}] = {}", i, c);
         if c.eq(&',') || i == 0 {
             comma_ind = i;
-            // println!("stop at {}", i);
             break;
         }
         else {
@@ -50,15 +46,13 @@ fn get_count_for_color(turn: &str, color_ind: usize) -> u8 {
         num_string.push(turn.chars().nth(j).unwrap());
     }
     
-    // println!("num_string = {:?}", num_string);
     let count: u8 = num_string.trim().parse().unwrap();
-    // println!("count = {}", count);
     count
 }
 
 fn main() {
     
-    let path = Path::new("./files/day2.txt");
+    let path = Path::new("../files/day2.txt");
     let mut file = File::open(&path).unwrap();
     let mut data_string = String::new();
     file.read_to_string(&mut data_string).unwrap();
@@ -71,48 +65,67 @@ fn main() {
     
     let colors = vec!["red", "green", "blue"];
     let mut total: u16 = 0;
+    let mut total_power: u32 = 0;
 
     for game in &lines {
-        // println!("---------------------------");
-        // println!("{}", game);
+        println!("{}", game);
+
+        let mut red_count_game: Vec<u16> = vec![];
+        let mut green_count_game: Vec<u16> = vec![];
+        let mut blue_count_game: Vec<u16> = vec![];
+
         let id: u16 = get_id_from_line(&game);
-        // println!("id = {}", id);
         let colon_ind = game.find(":").unwrap();
         let game_str = &game[colon_ind+1..];
         let split_game: Vec<&str> = game_str.split(";").collect();
 
         let mut possible = false;
         for turn in &split_game {
-            // println!("Turn = {}", turn);
-            let mut red_count: u8 = 0;
-            let mut green_count: u8 = 0;
-            let mut blue_count: u8 = 0;
+            let mut red_count_turn: u8 = 0;
+            let mut green_count_turn: u8 = 0;
+            let mut blue_count_turn: u8 = 0;
             for color in &colors {
                 let ind: Vec<_> = turn.match_indices(*color).collect();
                 for (i, v) in &ind {
-                    // println!("{}, {}", i, v);
                     let value: u8 = get_count_for_color(&turn, *i);
                     match v {
-                        &"red" => red_count = value,
-                        &"green" => green_count = value,
-                        &"blue" => blue_count = value,
+                        &"red" => red_count_turn = value,
+                        &"green" => green_count_turn = value,
+                        &"blue" => blue_count_turn = value,
                         _ => panic!("ahhh")
                     };
                 }
             }
-            if red_count > N_RED || green_count > N_GREEN || blue_count > N_BLUE {
-                possible = false;
-                // println!("Impossible");
-                break;
-            }
-            else {
-                possible = true;
-                // println!("Possible");
-            }
+            red_count_game.push(red_count_turn.into());
+            green_count_game.push(green_count_turn.into());
+            blue_count_game.push(blue_count_turn.into());
+            // if red_count_turn > N_RED || green_count_turn > N_GREEN || blue_count_turn > N_BLUE {
+            //     possible = false;
+            //     break;
+            // }
+            // else {
+            //     possible = true;
+            // }
         }
-        if possible {
-            total = total + id;
-        }
+        // if possible {
+        //     total = total + id;
+        // }
+         
+        println!("Red = {:?}", red_count_game);
+        println!("Green = {:?}", green_count_game);
+        println!("Blue = {:?}", blue_count_game);
+        let max_red: u16 = *red_count_game.iter().max().expect("hmm1") as u16;
+        let max_green: u16 = *green_count_game.iter().max().expect("hmm2") as u16;
+        let max_blue: u16 = *blue_count_game.iter().max().expect("hmm3") as u16;
+        println!("Max red = {}", max_red);
+        println!("Max green = {}", max_green);
+        println!("Max blue = {}", max_blue);
+        let power = max_red * max_green * max_blue;
+        println!("Power =  {}", power);
+        total_power += power as u32;
+        println!("Total power = {}" ,total_power);
+        println!("--------------------------------");
+
     }
-    println!("total = {}", total);
+    println!("\nTotal power = {}", total_power);
 }
